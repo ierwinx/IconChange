efb89d3860972aa801b67446def98397047b5a5e
Eяw!и Gontier <ierwinx@me.com>
2022-10-18 17:58:23 -0500

    Initial Commit

---
+++ IconChange/IconChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconChangeApp.swift
+//  IconChange
+//
+//  Created by Erwin Luz León on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct IconChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
